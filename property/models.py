60e8df30e1a8864d8ae5420926d4d1c2dcfcd8af
Aleksandr <menshikov.a.v@icloud.com>
2021-09-26 21:10:15 +0300

    Add model for owners

--- property/models.py
+++ property/models.py
@@ -6,7 +6,7 @@ from phonenumber_field.modelfields import PhoneNumberField
 
 
 class Flat(models.Model):
-    owner = models.CharField('ФИО владельца', max_length=200)
+    owner = models.CharField('ФИО владельца', max_length=200, db_index=True)
     owners_phonenumber = models.CharField('Номер владельца', max_length=20)
     owner_pure_phone = PhoneNumberField(verbose_name='Нормализованный номер '
                                                      'владельца',
@@ -78,3 +78,18 @@ class Claim(models.Model):
                              on_delete=models.SET_NULL,
                              null=True)
     claim_text = models.TextField('Текст жалобы')
+
+
+class Owner(models.Model):
+    name = models.CharField('ФИО владельца', max_length=200, db_index=True)
+    phonenumber = models.CharField('Номер владельца', max_length=20)
+    pure_phone = PhoneNumberField(verbose_name='Нормализованный номер '
+                                               'владельца',
+                                  null=True,
+                                  blank=True)
+    flats = models.ManyToManyField(Flat, verbose_name='Квартиры в '
+                                                      'собственности',
+                                   related_name='owners', db_index=True)
+
+    def __str__(self):
+        return self.name
